a617df11c5b6bc0ee219a0ca03b68abfe1de346b
Wannes Vantorre <vantorrewanes@gmail.com>
2024-08-06 14:05:11 +0200

    - refactor: Fix broken TestStringRuleFactory test
    - Fix broken TestStringRuleFactory unit test.
    - Adjust TestStringRuleFactory to work with new code.

--- utils/rule_factory_test.go
+++ utils/rule_factory_test.go
@@ -4,8 +4,17 @@ import (
 	"testing"
 )
 
-func TestGetAllRules(t *testing.T) {
-	factory := NewRuleFactory()
+func TestStringRuleFactory(t *testing.T) {
+	const abcRule string = `
+	rule abcRule {
+		meta: 
+			author = "Wannes Vantorre"
+		strings:
+			$str = "abc"
+		condition:
+			$str
+	}`
+	factory := NewRuleFactory([]string{abcRule})
 	_, err := factory.GetAllRules()
 	if err != nil {
 		t.Fatalf(`GetAllRules() error = %v`, err)

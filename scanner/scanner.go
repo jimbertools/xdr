d6ea70d0b1d15697db1ff62d012b5da598fa811d
Wannes Vantorre <vantorrewanes@gmail.com>
2024-08-06 12:25:28 +0200

    feature: Implement usefull ByteScanner type
    - Implement usefull ByteScanner type
    - Implement Scanner interface for ByteScanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,7 +1,19 @@
 package scanner
 
-import "github.com/hillu/go-yara/v4"
+import (
+	"github.com/hillu/go-yara/v4"
+)
 
 type Scanner interface {
-	Scan(rules *yara.Rules) (string, error)
+	Scan(rules *yara.Rules) (yara.MatchRules, error)
+}
+
+type BytesScanner struct {
+	bytes []byte
+}
+
+func (scanner *BytesScanner) Scan(rules *yara.Rules) (yara.MatchRules, error) {
+	var matches yara.MatchRules
+	err := rules.ScanMem(scanner.bytes, 0, 0, &matches)
+	return matches, err
 }

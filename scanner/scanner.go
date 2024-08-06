6523979ebdb80121eada5cf0a60130335825b639
Wannes Vantorre <vantorrewanes@gmail.com>
2024-08-06 13:02:17 +0200

    feature: Implement required FileScanner type
    - Implement FileScanner type.
    - Implement NewFileScanner utilitity function.
    - Implement Scanner interface for FileScanner.

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -23,3 +23,19 @@ func (scanner *BytesScanner) Scan(rules *yara.Rules) (yara.MatchRules, error) {
 	err := rules.ScanMem(scanner.bytes, 0, 0, &matches)
 	return matches, err
 }
+
+type FileScanner struct {
+	filePath string
+}
+
+func NewFileScanner(filePath string) *FileScanner {
+	return &FileScanner{
+		filePath: filePath,
+	}
+}
+
+func (scanner *FileScanner) Scan(rules *yara.Rules) (yara.MatchRules, error) { 
+	var matches yara.MatchRules
+	err := rules.ScanFile(scanner.filePath, 0, 0, &matches)
+	return matches, err
+}

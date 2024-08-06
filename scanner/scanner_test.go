1b03e6fdc04677d68f24d2411fdc3376468d3b2e
Wannes Vantorre <vantorrewanes@gmail.com>
2024-08-06 13:45:30 +0200

    feature: Implement required FileScanner test
    - feature: Implement required FileScanner unit test.
    - Add test data as a file for FileScanner tests.
    - Rename abigious test to correct name.

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -1,13 +1,14 @@
 package scanner_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/vantorrewannes/file-scanner/scanner"
 	"github.com/vantorrewannes/file-scanner/utils"
 )
 
-func TestGetAllRules(t *testing.T) {
+func TestByteScanner(t *testing.T) {
 	factory := utils.NewRuleFactory()
 	rules, err := factory.GetAllRules()
 	if err != nil {
@@ -17,5 +18,27 @@ func TestGetAllRules(t *testing.T) {
 	_, err = scanner.Scan(rules)
 	if err != nil {
 		t.Fatalf(`Scan() error = %v`, err)
+	}
+}
+
+func TestFileScanner(t *testing.T) {
+	factory := utils.NewRuleFactory()
+	rules, err := factory.GetAllRules()
+	if err != nil {
+		t.Fatalf(`GetAllRules() error = %v`, err)
+	}
+	scanner := scanner.NewFileScanner("../test_files/test.txt")
+	result, err := scanner.Scan(rules)
+	if err != nil {
+		t.Fatalf(`Scan() error = %v`, err)
+	}
+	if len(result) != 2 {
+		t.Fatalf(`Scan() error = %v`, fmt.Errorf("expected 2 results, got %d", len(result)))
+	}
+	if result[0].Rule != "abcRule" {
+		t.Fatalf(`Scan() error = %v`, fmt.Errorf("expected abcRule, got %s", result[0].Rule))
+	}
+	if result[1].Rule != "xyzRule" {
+		t.Fatalf(`Scan() error = %v`, fmt.Errorf("expected xyzRule, got %s", result[1].Rule))
 	}
 }

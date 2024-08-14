aa188b6451925492837de3426d439f7cdcf5db22
VantorreWannes <vantorrewannes@gmail.com>
2024-08-14 12:18:54 +0200

    feature: Convert into proper cli program
    - Convert library into proper useful cli program.

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"path/filepath"
 
 	"github.com/hillu/go-yara/v4"
 	"github.com/jimbertools/volmgmt/usn"
@@ -12,20 +14,43 @@ import (
 )
 
 func main() {
-	const ruleFilePath = "rules.yar"
-	yaraScanner, err := scanner.YaraScannerFromRuleFile(ruleFilePath)
+	var (
+		yaraRulesPath string
+		reason        string
+	)
+	flag.StringVar(&yaraRulesPath, "path", "", "The path to the yara rules file")
+	flag.StringVar(&reason, "event", "", "The event that will trigger the watcher to scan the file.")
+
+	flag.Parse()
+
+	if yaraRulesPath == "" {
+		log.Fatal("Yara file path is required")
+	}
+	if reason == "" {
+		log.Fatal("Event is required")
+	}
+
+	usnReason, err := usn.ParseReason(reason)
+	if err != nil {
+		log.Fatal(err)
+	}
+	path, err := filepath.Abs(yaraRulesPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	yaraScanner, err := scanner.YaraScannerFromRuleFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
 	watcher := watcher.NewWatcher(yaraScanner, onYaraMatch, onNoYaraMatch)
-	err = watcher.Watch(journal.C, usn.ReasonFileCreate, ctx)
+	err = watcher.Watch(journal.C, usnReason, ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func onYaraMatch(record usn.Record, path string, matches *yara.MatchRules) { 
+func onYaraMatch(record usn.Record, path string, matches *yara.MatchRules) {
 	log.Println("VIRUS:", path)
 }
 

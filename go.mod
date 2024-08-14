218ce9e364da7d362576426fb65bed62dc31a6b1
VantorreWannes <vantorrewannes@gmail.com>
2024-08-14 11:18:43 +0200

    feature: Implement usefull disk journal monitor
    - Implement usefull disk journal monitor

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/vantorrewannes/watcheye
 
 go 1.22.6
 
-require github.com/hillu/go-yara/v4 v4.3.3
+require (
+	github.com/hillu/go-yara/v4 v4.3.3
+	github.com/jimbertools/volmgmt v0.0.0-20240809131335-b54f62d194e4
+)
+
+require golang.org/x/sys v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/jimbertools/volmgmt/usn"
+	"github.com/vantorrewannes/watcheye/pkg/watcheye/disk/journal"
+)
+
+func main() {
+	diskLetter := journal.C
+	journal, err := journal.NewJournal(diskLetter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	recordChannel, _, err := journal.Listen(usn.ReasonAny, ctx, nil, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for record := range recordChannel {
+		log.Println(record.FileName)
+	}
 }
